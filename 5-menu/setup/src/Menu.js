f62b6070e0ad23188d0c20df2f8d5f822f3d378f
dm1tro69 <dimolg22@gmail.com>
2021-01-24 14:14:08 +0200

    add tabs

--- 5-menu/setup/src/Menu.js
+++ 5-menu/setup/src/Menu.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({menuItems}) => {
+  return <div className={'section-center'}>
+    {menuItems.map((item)=> {
+      const {id, desc, img, price, title}= item
+      return (
+           <article key={id} className={'menu-item'}>
+             <img src={img} alt="img" className={'photo'}/>
+             <div className="item-info">
+               <header>
+                 <h4>{title}</h4>
+                 <h4 className={price}>{price}</h4>
+               </header>
+               <p className={'item-text'}>{desc}</p>
+             </div>
+           </article>
+      )
+    })}
+  </div>;
 };
 
 export default Menu;
